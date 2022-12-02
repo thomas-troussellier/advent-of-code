dc5eedf601b93416fa37ad32163a3ec0c4e5b794
Troussellier Thomas <thomas.troussellier@batch.com>
2022-12-02 09:45:14 +0100

    aoc2022: day2

--- day_1.go
+++ day_1.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -30,4 +35,63 @@ func Day1() {
 
 	fmt.Println("Q1. Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?")
 	fmt.Printf("A2. Top three elves are %s, with %d total calories\n", strings.Join(chadElves, ","), cumulativeLoad)
+}
+
+type elf struct {
+	name  string
+	load  []int
+	total int
+}
+
+func (e elf) String() string {
+	return fmt.Sprintf("name: %s, total: %d, load: %v\n", e.name, e.total, e.load)
+}
+
+func (e *elf) AddLoad(calories int) {
+	e.load = append(e.load, calories)
+}
+
+type elves []*elf
+
+func (e elves) Sort(sortFunc func(i, j int) bool) {
+	sort.SliceStable(e, sortFunc)
+}
+
+func NewElf(name string) *elf {
+	return &elf{name: name}
+}
+
+func LoadElvesData(fileName string) elves {
+	readFile, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	elves := make(elves, 0, 10)
+	elvesCount := 0
+	currentElf := NewElf("1")
+
+	for fileScanner.Scan() {
+		currentLine := fileScanner.Text()
+
+		if len(currentLine) == 0 {
+			elvesCount += 1
+			elves = append(elves, currentElf)
+			currentElf = NewElf(strconv.Itoa(elvesCount + 1))
+		} else {
+			intLoad, err := strconv.Atoi(currentLine)
+			if err != nil {
+				log.Fatal("invalid input")
+			}
+			currentElf.AddLoad(intLoad)
+			currentElf.total += intLoad
+		}
+	}
+	readFile.Close()
+
+	return elves
 }
--- main.go
+++ main.go
@@ -1,72 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"sort"
-	"strconv"
 )
-
-type elf struct {
-	name  string
-	load  []int
-	total int
-}
-
-func (e elf) String() string {
-	return fmt.Sprintf("name: %s, total: %d, load: %v\n", e.name, e.total, e.load)
-}
-
-func (e *elf) AddLoad(calories int) {
-	e.load = append(e.load, calories)
-}
-
-type elves []*elf
-
-func (e elves) Sort(sortFunc func(i, j int) bool) {
-	sort.SliceStable(e, sortFunc)
-}
-
-func NewElf(name string) *elf {
-	return &elf{name: name}
-}
-
-func LoadElvesData(fileName string) elves {
-	readFile, err := os.Open(fileName)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	fileScanner := bufio.NewScanner(readFile)
-
-	fileScanner.Split(bufio.ScanLines)
-
-	elves := make(elves, 0, 10)
-	elvesCount := 0
-	currentElf := NewElf("1")
-
-	for fileScanner.Scan() {
-		currentLine := fileScanner.Text()
-
-		if len(currentLine) == 0 {
-			elvesCount += 1
-			elves = append(elves, currentElf)
-			currentElf = NewElf(strconv.Itoa(elvesCount + 1))
-		} else {
-			intLoad, err := strconv.Atoi(currentLine)
-			if err != nil {
-				log.Fatal("invalid input")
-			}
-			currentElf.AddLoad(intLoad)
-			currentElf.total += intLoad
-		}
-	}
-	readFile.Close()
-
-	return elves
-}
 
 func main() {
 	fmt.Println("~~~~~~~~~~~~~~~~~~~")
@@ -74,4 +10,5 @@ func main() {
 	fmt.Println("~~~~~~~~~~~~~~~~~~~")
 
 	Day1()
+	Day2()
 }
